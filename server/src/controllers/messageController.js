ec7d7b254fdbcb14a24584e05663181f24515022
Prasanna Shrestha <shresthaprasanna230@gmail.com>
2025-07-28 10:32:39 +0545

    complete message api funcitionality

--- server/src/controllers/messageController.js
+++ server/src/controllers/messageController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel";
 import Message from "./../models/messageModel";
+import cloudinary from "./../lib/cloudinary";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -21,7 +22,50 @@ export const getUsersForSidebar = async (req, res) => {
 export const getMessages = async (req, res) => {
   try {
     const { id: userToChatId } = req.params;
-    const sender
+    const currentUserId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: currentUserId, receiverId: userToChatId },
+        { receiverId: currentUserId, senderId: userToChatId },
+      ],
+    });
+    return res.status(200).json({
+      messages,
+      message: "Fetched Message Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { id: receiverId } = req.params;
+    const { text, image } = req.body;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      //upload base64 image to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    return res.status(201).json({
+      newMessage,
+      message: "Message Created Successfully",
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
--- server/src/routes/messageRoute.js
+++ server/src/routes/messageRoute.js
@@ -3,11 +3,13 @@ import { protectRoute } from "../middleware/authMiddleware.js";
 import {
   getMessages,
   getUsersForSidebar,
+  sendMessage,
 } from "../controllers/messageController.js";
 
 const router = express.Router();
 
 router.get("/getUsers", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
+router.get("/send/:id", protectRoute, sendMessage);
 
 export default router;
